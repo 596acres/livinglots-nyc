bad8a5d2d0fc749879f24daf92f921df7e1e00ef
Eric Brelsford <ebrelsford@gmail.com>
2014-10-01 20:04:00 -0400

    TileStache deploy

---
+++ deploy/templates/tilestache_start_gunicorn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+WEBAPP_NAME="<WEBAPP_NAME>"
+PORT=<PORT>
+USER=$USER
+GROUP=$USER
+NUM_WORKERS=3
+DJANGO_WSGI_MODULE=$DJANGO_PROJECT_NAME.wsgi
+VIRTUALENV=$WEBAPP_NAME
+LOGDIR=$HOME/webapps/$WEBAPP_NAME/logs
+
+echo "Starting $WEBAPP_NAME"
+
+# Activate the virtual environment
+source ~/bin/virtualenvwrapper.sh
+workon $VIRTUALENV
+
+cd $HOME/webapps/$WEBAPP_NAME
+
+# Start Django Unicorn
+exec gunicorn \
+  --name $WEBAPP_NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --log-level=debug \
+  --bind=127.0.0.1:$PORT \
+  --log-file $LOGDIR/error.log --access-logfile $LOGDIR/access.log \
+  "TileStache:WSGITileServer('$HOME/webapps/$WEBAPP_NAME/tilestache.cfg')"
