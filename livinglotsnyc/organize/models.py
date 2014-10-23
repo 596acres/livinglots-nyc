d7a201860edb9fc172eced19bf270f060ad77f9a
Eric Brelsford <ebrelsford@gmail.com>
2014-10-23 12:53:45 -0400

    get_edit_url

--- livinglotsnyc/organize/models.py
+++ livinglotsnyc/organize/models.py
@@ -1,5 +1,12 @@
+from django.core.urlresolvers import reverse
+
 from livinglots_organize.models import BaseOrganizer
 
 
 class Organizer(BaseOrganizer):
-    pass
+
+    def get_edit_url(self):
+        return reverse('organize:edit_participant', kwargs={
+            'hash': self.email_hash,
+            'pk': self.object_id,
+        })
