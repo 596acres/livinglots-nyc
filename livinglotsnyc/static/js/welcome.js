b7773851ac3e3ca20b439044b2c874de9182d72f
Eric Brelsford <ebrelsford@gmail.com>
2014-11-12 19:16:09 -0500

    Make welcome header less extreme

--- livinglotsnyc/static/js/welcome.js
+++ livinglotsnyc/static/js/welcome.js
@@ -5,7 +5,6 @@
 function initClose() {
     $('.map-welcome-close-button').click(function (e) {
         $('.map-welcome').addClass('closed');
-        $('.map-welcome h1').animate({ 'font-size': '28px' });
         $('.map-welcome-body').slideUp();
         e.preventDefault();
         return false;
@@ -15,7 +14,6 @@ function initClose() {
 function initOpen() {
     $('.map-welcome-open-button').click(function (e) {
         $('.map-welcome').removeClass('closed');
-        $('.map-welcome h1').animate({ 'font-size': '56px' });
         $('.map-welcome-body').slideDown();
         e.preventDefault();
         return false;
