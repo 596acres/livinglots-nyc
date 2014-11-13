b664ac1c4380c2fca5efa5962bc7c09b6595927c
Eric Brelsford <ebrelsford@gmail.com>
2014-11-13 13:45:06 -0500

    Make an way to subscribe without an object

--- livinglotsnyc/newsletter/subscribe.py
+++ livinglotsnyc/newsletter/subscribe.py
@@ -6,7 +6,34 @@ import mailchimp
 from mailchimp.chimpy.chimpy import ChimpyException
 
 
-def subscribe(obj, is_participating=False):
+def subscribe(email, first_name=None, last_name=None, is_participating=False,
+              **kwargs):
+    """Subscribe the given email to the mailing list."""
+    merge_dict = {
+        'EMAIL': email,
+    }
+
+    if is_participating:
+        merge_dict['GROUPINGS'] = [settings.MAILCHIMP_PARTCICIPATION_GROUP,]
+
+    merge_dict['FNAME'] = first_name
+    merge_dict['LNAME'] = last_name
+
+    if settings.DEBUG:
+        logging.debug('Would be subscribing %s to the mailing list with '
+                      'merge_dict %s' % (email, merge_dict,))
+        return
+
+    try:
+        mailchimp.utils.get_connection() \
+                .get_list_by_id(settings.MAILCHIMP_LIST_ID) \
+                .subscribe(email, merge_dict)
+    except ChimpyException:
+        # Thrown if user already subscribed--ignore
+        return
+
+
+def subscribe_obj(obj, **kwargs):
     """
     Subscribe an object representing a person to the mailing list.  Assumes 
     there will be an email attribute on the object.
@@ -14,30 +41,14 @@ def subscribe(obj, is_participating=False):
     if not obj or not obj.email:
         return
 
-    merge_dict = {
-        'EMAIL': obj.email,
-    }
-
-    if is_participating:
-        merge_dict['GROUPINGS'] = [settings.MAILCHIMP_PARTCICIPATION_GROUP,]
+    if not kwargs:
+        kwargs = {}
 
     if obj.name:
         try:
             first, last = obj.name.split()
-            merge_dict['FNAME'] = first
-            merge_dict['LNAME'] = last
+            kwargs['first_name'] = first
+            kwargs['last_name'] = last
         except Exception:
-            merge_dict['FNAME'] = obj.name
-
-    if settings.DEBUG:
-        logging.debug('Would be subscribing %s to the mailing list with '
-                      'merge_dict %s' % (obj.email, merge_dict,))
-        return
-
-    try:
-        mailchimp.utils.get_connection() \
-                .get_list_by_id(settings.MAILCHIMP_LIST_ID) \
-                .subscribe(obj.email, merge_dict)
-    except ChimpyException:
-        # Thrown if user already subscribed--ignore
-        return
+            kwargs['first_name'] = obj.name
+    subscribe(obj.email, **kwargs)
