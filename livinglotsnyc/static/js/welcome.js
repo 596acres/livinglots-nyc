cf234953d1360402d927b73c57887d1b32b923b5
Eric Brelsford <ebrelsford@gmail.com>
2015-02-05 16:05:06 -0500

    Whole header toggles welcome text

--- livinglotsnyc/static/js/welcome.js
+++ livinglotsnyc/static/js/welcome.js
@@ -2,27 +2,13 @@
 // Welcome header
 //
 
-function initClose() {
-    $('.map-welcome-close-button').click(function (e) {
-        $('.map-welcome').addClass('closed');
-        $('.map-welcome-body').slideUp();
-        e.preventDefault();
-        return false;
-    });
-}
-
-function initOpen() {
-    $('.map-welcome-open-button').click(function (e) {
-        $('.map-welcome').removeClass('closed');
-        $('.map-welcome-body').slideDown();
-        e.preventDefault();
-        return false;
-    });
-}
-
 module.exports = {
     init: function () {
-        initClose();
-        initOpen();
+        $('.map-welcome-toggle').click(function (e) {
+            $('.map-welcome').toggleClass('closed');
+            $('.map-welcome-body').slideToggle();
+            e.preventDefault();
+            return false;
+        });
     }
 };
