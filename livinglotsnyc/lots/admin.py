f8536aa24f51b1eefe433c42d6053f7844b42e72
Eric Brelsford <ebrelsford@gmail.com>
2015-03-16 22:47:57 -0400

    Use autocomplete for owners

--- livinglotsnyc/lots/admin.py
+++ livinglotsnyc/lots/admin.py
@@ -1,11 +1,27 @@
+from django import forms
 from django.contrib import admin
 
+from autocomplete_light import ChoiceWidget
+
+from livinglots_forms.widgets import AddAnotherWidgetWrapper
 from livinglots_lots.admin import BaseLotAdmin
 
+from owners.models import Owner
 from .models import Lot
 
 
+class LotAdminForm(forms.ModelForm):
+    owner = forms.ModelChoiceField(
+        queryset=Owner.objects.all().order_by('name'),
+        widget=AddAnotherWidgetWrapper(ChoiceWidget('OwnerAutocomplete'), Owner)
+    )
+
+    class Meta:
+        model = Lot
+
+
 class LotAdmin(BaseLotAdmin):
+    form = LotAdminForm
     fieldsets = (
             ('NYC specific', {
                 'fields': ('bbl',
