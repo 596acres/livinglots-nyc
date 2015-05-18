f7ca4e925a8cce7eb21fccce24f3bd3a36690592
Eric Brelsford <ebrelsford@gmail.com>
2015-05-18 10:38:07 -0400

    Fall back to garden id if no bbl for OASIS links

--- livinglotsnyc/lots/templatetags/lots_tags.py
+++ livinglotsnyc/lots/templatetags/lots_tags.py
@@ -11,6 +11,28 @@ register = template.Library()
 PLUTO_URL = 'http://www.nyc.gov/html/dcp/html/bytes/applbyte.shtml#pluto'
 
 
+class GetOasisUrl(AsTag):
+    options = Options(
+        'for',
+        Argument('lot', resolve=True, required=True),
+        'as',
+        Argument('varname', resolve=False, required=False),
+    )
+
+    def get_value(self, context, lot):
+        base = 'http://oasisnyc.net/map.aspx?etabs=1&zoomto='
+        if lot.bbl and not lot.bbl_is_fake:
+            return '%slot:%s' % (base, lot.bbl)
+        try:
+            return '%sgarden:%s' % (
+                base,
+                lot.steward_projects.all()[0].external_id,
+            )
+        except Exception:
+            pass
+        return None
+
+
 class GetVacantReasons(AsTag):
     options = Options(
         'for',
@@ -47,4 +69,5 @@ class GetVacantReasons(AsTag):
         return list(set(reasons))
 
 
+register.tag(GetOasisUrl)
 register.tag(GetVacantReasons)
